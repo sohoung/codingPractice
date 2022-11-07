e1fc635b498f3494ab779e7f6d9f11c472a63dc9
김민성 <sohoung4@gmail.com>
2022-11-07 20:52:33 +0900

    회문문자열

--- src/String/회문문자열.java
+++ src/String/회문문자열.java
@@ -15,7 +15,7 @@ public class 회문문자열 {
         int len = str.length();   // for문에 따른 charAt의 값을 변화해주기 위해서 입력받은 str의 길이를 구해준다.
         for(int i = 0; i < len/2; i++) {  // for문으로 돌려주는데 조건문을 i < len/2로 한 이유는 서로의 문자를 비교하고 가운데에서는 더 이상 비교할 필요가 없으므로
                                           // len/2로 반복횟수를 정해준다.
-            if(str.charAt(i) != str.charAt(len - i - 1)) {  // if문으로 str.charAt의 값들을 비교하여 같지않다면 return값으로 NO를 해주고 for문을 종료시킨다.
+            if(str.charAt(i) != str.charAt(len - i - 1)) {  // if문으로 str.charAt의 값들을 비교하여 같지않다면 return값으로 NO를 해주고 for문을 종료시킨다.팰리
                 return "NO";
             }
         }
