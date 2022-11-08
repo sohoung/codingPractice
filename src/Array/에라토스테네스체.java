f12e5f2559ec74b86880fd1388d0dbb924de9c8d
김민성 <sohoung4@gmail.com>
2022-11-08 20:52:37 +0900

    에라토스테네스 체

---
+++ src/Array/에라토스테네스체.java
@@ -0,0 +1,20 @@
+package Array;
+import java.util.Scanner;
+
+// 설명 : 자연수 N이 입력되면 1부터 N까지의 소수의 개수를 출력하는 프로그램을 작성하세요.
+//       만약 20이 입력되면 1부터 20까지의 소수는 2, 3, 5, 7, 11, 13, 17, 19로 총 8개입니다.
+// 입력 예시 : 20
+// 출력 예시 : 8
+public class 에라토스테네스체 {
+    public int solution(int n) {
+        int answer = 0;
+        
+        return answer;
+    }
+    public static void main(String[] args) {
+         에라토스테네스체 m = new 에라토스테네스체();
+         Scanner sc = new Scanner(System.in);
+         int n = sc.nextInt();
+        System.out.println(m.solution(n));
+    }
+}
