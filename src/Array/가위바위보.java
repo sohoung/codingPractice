377918aa10fdc3b4bda9019f26b6b957dc4439b0
김민성 <sohoung4@gmail.com>
2022-11-08 20:52:27 +0900

    가위바위보

--- src/Array/가위바위보.java
+++ src/Array/가위바위보.java
@@ -15,22 +15,40 @@ import java.util.Scanner;
 public class 가위바위보 {
     public String solution(int n, int[] a, int[] b) {
         String answer = "";
-        
+        for(int i = 0; i < n; i++) {
+            if(a[i] == b[i]) {  // a와 b가 비긴경우
+                answer += "D";
+            }
+            else if(a[i] == 1 && b[i] == 3) {  // a가 이긴 경우 1
+                answer += "A";
+            }
+            else if(a[i] == 3 && b[i] == 2) {  // a가 이긴 경우 2
+                answer += "A";
+            }
+            else if(a[i] == 2 && b[i] == 1) {  // a가 이긴 경우 3
+                answer += "A";
+            }
+            else {                             // a와 b가 비기지도 않고 a가 이기지도 않았으면 나머지 모든 경우의 수는 b가 이긴 경우이다.
+                answer += "B";
+            }
+        }
         return answer;
     }
 
     public static void main(String[] args) {
         가위바위보 m = new 가위바위보();
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] a = new int[n];
-        int[] b = new int[n];
-        for(int i = 0; i < n; i++) {
+        int n = sc.nextInt();  // 게임 횟수
+        int[] a = new int[n];  // 게임 횟수에 따른 배열 동적 할당
+        int[] b = new int[n];  // 게임 횟수에 따른 배열 동적 할당
+        for(int i = 0; i < n; i++) {  // a가 낸 가위바위보
             a[i] = sc.nextInt();
         }
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++) {  // b가 낸 가위바위보
             b[i] = sc.nextInt();
         }
-        System.out.println(m.solution(n,a,b));
+        for(char c : m.solution(n,a,b).toCharArray()){  // String이므로 출력형식이 줄바꿈이 되어있으므로 향상된 for문으로 배열로 출력형식을 맞춰준다.
+            System.out.println(c);
+        }
     }
 }
