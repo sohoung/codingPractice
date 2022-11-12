0b5a9f7f0d2659c99b440143a555a90b15315a74
김민성 <sohoung4@gmail.com>
2022-11-12 18:40:06 +0900

    매출액의종류

---
+++ src/HashMap_TreeSet/매출액의종류.java
@@ -0,0 +1,40 @@
+package HashMap_TreeSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+// 설명 : 현수의 아빠는 제과점을 운영합니다. 현수아빠는 현수에게 N일 동안의 매출기록을 주고 연속된 K일 동안의 매출액의 종류를
+//       각 구간별로 구하라고 했습니다.
+//       만약 N=7이고 7일 간의 매출기록이 아래와 같고, 이때 K=4이면
+//       20 12 20 10 23 17 10
+//       각 연속 4일간의 구간의 매출종류는
+//       첫 번째 구간은 [20, 12, 20, 10]는 매출액의 종류가 20, 12, 10으로 3이다.
+//       두 번째 구간은 [12, 20, 10, 23]는 매출액의 종류가 4이다.
+//       세 번째 구간은 [20, 10, 23, 17]는 매출액의 종류가 4이다.
+//       네 번째 구간은 [10, 23, 17, 10]는 매출액의 종류가 3이다.
+//       N일간의 매출기록과 연속구간의 길이 K가 주어지면 첫 번째 구간부터 각 구간별
+//       매출액의 종류를 출력하는 프로그램을 작성하세요.
+// 입력 예시 : 7 4
+//          20 12 20 10 23 17 10
+// 출력 예시 : 3 4 4 3
+public class 매출액의종류 {
+    public ArrayList<Integer> solution(int[] arr, int n, int k) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        매출액의종류  m = new 매출액의종류();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for(int x : m.solution(arr,n,k)) {
+            System.out.print(x + " ");
+        }
+    }
+}
