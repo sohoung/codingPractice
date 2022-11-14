e941763d90977315ad5b4bf4ab6b35be5dd52c89
김민성 <sohoung4@gmail.com>
2022-11-14 20:09:44 +0900

    괄호 문자 제거

---
+++ src/Stack_Queue/괄호문자제거.java
@@ -0,0 +1,13 @@
+package Stack_Queue;
+import java.util.Scanner;
+
+// 설명 : 입력된 문자열에서 소괄호 ( ) 사이에 존재하는 모든 문자를 제거하고 남은 문자만 출력하는 프로그램을 작성하세요.
+// 입력 예시 : (A(BC)D)EF(G(H)(IJ)K)LM(N)
+// 출력 예시 : EFLM
+public class 괄호문자제거 {
+
+    public static void main(String[] args) {
+        괄호문자제거 m  = new 괄호문자제거();
+        Scanner sc = new Scanner(System.in);
+    }
+}
