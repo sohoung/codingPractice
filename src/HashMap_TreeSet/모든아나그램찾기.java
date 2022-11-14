03b6e3b343bc7347a43159f9fd84a6b4eadeb26f
김민성 <sohoung4@gmail.com>
2022-11-14 19:11:19 +0900

    모든 아나그램 찾기

--- src/HashMap_TreeSet/모든아나그램찾기.java
+++ src/HashMap_TreeSet/모든아나그램찾기.java
@@ -11,23 +11,24 @@ import java.util.Scanner;
 public class 모든아나그램찾기 {
     public int solution(String a, String b) {
         int answer = 0;
-        HashMap<Character,Integer> am = new HashMap<>();
-        HashMap<Character,Integer> bm = new HashMap<>();
-        for(char x : b.toCharArray()) {
+        HashMap<Character,Integer> am = new HashMap<>();  // 입력받은 a에 대한 HashMap 객체 생성
+        HashMap<Character,Integer> bm = new HashMap<>();  // 입력받은 b에 대한 HashMap 객체 생성
+        for(char x : b.toCharArray()) {  //  먼저 b에 대한 값을 bm에 넣어준다.
             bm.put(x,bm.getOrDefault(x,0)+1);
         }
-        int lt = 0;
-        int L = b.length()-1;
-        for(int i = 0; i < L; i++) {
+        int lt = 0;  // 시작 index지정
+        int L = b.length()-1;  // a의 세팅을 위한 변수
+        for(int i = 0; i < L; i++) {  // b의 길이보다 1작은 값으로 am의 데이터를 먼저 세팅한다.
             am.put(a.charAt(i), am.getOrDefault(a.charAt(i),0)+1);
         }
         for(int rt = L; rt < a.length(); rt++) {
+            // 먼저 세팅된 am의 값에 rt index의 값을 추가하여 rt와 lt의 index에 대한 변화를 주면서 문장이 b와 아나그램이 되는지 확인한다.
             am.put(a.charAt(rt),am.getOrDefault(a.charAt(rt),0)+1);
             if(am.equals(bm)) {
                 answer++;
             }
             am.put(a.charAt(lt),am.get(a.charAt(lt))-1);
-            if(am.get(a.charAt(lt)) == 0) {
+            if(am.get(a.charAt(lt)) == 0) {  // 0의 값이 있으면 value값이 0으로 된다고 생각하므로 지워준다.
                 am.remove(a.charAt(lt));
             }
             lt++;
