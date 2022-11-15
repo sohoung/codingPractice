59911da565e8b775ba3db3502167fbb1dbcd0543
김민성 <sohoung4@gmail.com>
2022-11-15 20:08:26 +0900

    공주구하기

--- src/Stack_Queue/공주구하기.java
+++ src/Stack_Queue/공주구하기.java
@@ -1,5 +1,7 @@
 package Stack_Queue;
 import java.sql.SQLOutput;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 // 설명 : 정보 왕국의 이웃 나라 외동딸 공주가 숲속의 괴물에게 잡혀갔습니다.
@@ -22,7 +24,19 @@ import java.util.Scanner;
 public class 공주구하기 {
     public int solution(int n, int k) {
         int answer = 0;
-
+        Queue<Integer> Q = new LinkedList<>();  // Queue의 객체 생성은 이렇게 LinkedList로 한다.
+        for(int i = 1; i <= n; i++) {
+            Q.offer(i);  // Queue에 1부터 n끼지의 데이터를 차례대로 삽입
+        }
+        while(!Q.isEmpty()) {  // while문으로 Q가 비어있지 않을때 수행하도록 조건을 걸어주고 Q가 비어있다면 while문의 수행을 멈추도록 해준다.
+            for(int i = 1; i < k; i++) {  // for문으로 Q에서 k-1번째까지의 숫자를 다시 Q에 넣어주고 k번째의 데이터는 Q에서 poll()해준다.
+                Q.offer(Q.poll());
+            }
+            Q.poll();
+            if(Q.size() == 1) {  // Q가 size가 1이 되었을 때 문제의 조건에 맞는 출력문이 나오므로 if문으로 Q의 size가 1이면 answer에 남아있는 Q의 값을 poll()해준다.
+                answer = Q.poll();  // poll()은 pop()와 같이 값을 다시 return해준다. 즉 answer에 poll()로 값을 넣어준 것처럼 데이터를 바로 넣어줄 수 있다는 말이다.
+            }
+        }
         return answer;
     }
     public static void main(String[] args) {
