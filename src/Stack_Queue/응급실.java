52791d1ef99514a3295774bc1c3c78eafa6475ef
김민성 <sohoung4@gmail.com>
2022-11-15 21:23:46 +0900

    응급실

--- src/Stack_Queue/응급실.java
+++ src/Stack_Queue/응급실.java
@@ -1,4 +1,6 @@
 package Stack_Queue;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 // 설명 : 메디컬 병원 응급실에는 의사가 한 명밖에 없습니다.
@@ -13,21 +15,52 @@ import java.util.Scanner;
 // 입력 예시 : 5 2
 //          60 50 70 80 90
 // 출력 예시 : 3
+// 입력 예시 : 6 3
+//          70 60 90 60 60 60
+// 출력 예시 : 4
+
+class Person {  // 환자의 순서와 우선순위에 대한 class Person
+    int id;  // 순서
+    int priority;  // 우선순위
+    public Person(int id, int priority) {
+        this.id = id;
+        this.priority = priority;
+    }
+}
 public class 응급실 {
     public int solution(int n, int[] arr, int k) {
         int answer = 0;
-        
+        Queue<Person> Q = new LinkedList<>();
+        for(int i = 0; i < n; i++) {  // Person객체를 이용하여 Q에 환자의 순서와 우선순위를 차례대로 삽입
+            Q.offer(new Person(i,arr[i]));
+        }
+        while(!Q.isEmpty()) {
+            Person tmp = Q.poll();  // Q의 데이터를 poll로 가져와 Person형 객체 tmp에 값을 넣어준다.
+            for(Person x : Q) {  // 현재의 환자와 대기목록의 환자의 우선순위를 비교하기 위해서 향상된 for문을 사용한다.
+                if(x.priority > tmp.priority) {  // 현재의 환자 우선순위보다 대기목록의 환자중에서 우선순위가 높은 환자가 있다면 현재의 환자를 다시 뒤로 넘기고 tmp값을 null로 바꿔주고 break해준다.
+                    Q.offer(tmp);
+                    tmp = null;
+                    break;
+                }
+            }
+            if(tmp != null) {  // tmp가 null이 아니란 말은 어떤 환자가 진료를 받을 수 있다는 소리이다. 진료를 받는 환자가 있을 때마다 count를 해줘야하므로 answer을 먼저 1증가시키고
+                answer++;
+                if(tmp.id == k) { // 만약 입력받은 k번째의 순서와 tmp.id 즉 현재 진료를 받을 수 있는 환자의 순서가 같다면 이것이 구하려고 하는 답이므로 answer에 값을 넣어준다.
+                    return answer;
+                }
+            }
+        }
         return answer;
     }
     public static void main(String[] args) {
         응급실  m = new 응급실();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int k = sc.nextInt();
         int[] arr = new int[n];
         for(int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        int k = sc.nextInt();
         System.out.println(m.solution(n,arr,k));
     }
 }
