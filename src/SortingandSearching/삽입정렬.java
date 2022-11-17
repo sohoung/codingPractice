cf2bb1484ce8d459d58ab8b3f477cbfb3827cec5
김민성 <sohoung4@gmail.com>
2022-11-17 20:09:32 +0900

    삽입정렬

--- src/SortingandSearching/삽입정렬.java
+++ src/SortingandSearching/삽입정렬.java
@@ -8,7 +8,19 @@ import java.util.Scanner;
 // 출력 예시 : 5 6 7 9 10 11
 public class 삽입정렬 {
     public int[] solution(int[] arr , int n) {
-        
+        for(int i = 1; i < n; i++) {
+            int tmp = arr[i];
+            int j;  // j for문의 범위 밖에서 j를 사용하기 때문에 j를 밖에서 따로 선언해준다.
+            for(j = i-1; j >= 0; j--){
+                if(arr[j] > tmp) {  // 앞의 데이터가 뒤의 데이터 값보다 크다면 앞의 데이터 값을 뒤의 index에 넣어준다.
+                    arr[j+1] = arr[j];
+                }
+                else {
+                    break;
+                }
+            }
+            arr[j+1] = tmp;  // j for문이 멈춘 지점 앞에서 tmp값을 넣어준다.
+        }
         return arr;
     }
     public static void main(String[] args) {
