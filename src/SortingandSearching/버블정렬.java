aa3c8a96a2c088ebacfe4a56bb0254d0c4f9f67a
김민성 <sohoung4@gmail.com>
2022-11-17 19:55:45 +0900

    버블정렬

--- src/SortingandSearching/버블정렬.java
+++ src/SortingandSearching/버블정렬.java
@@ -8,7 +8,15 @@ import java.util.Scanner;
 // 출력 예시 : 5 7 11 13 15 23
 public class 버블정렬 {
     public int[] solution(int[] arr, int n) {
-        
+        for(int i = 0; i < n-1; i++) {  // 몇 번을 실행할껀지의 for문
+            for(int j = 0; j < n-i-1; j++) {  // 비교 for문
+                if(arr[j] > arr[j+1]) {  // 만약 현재의 index 값과 뒤의 index 값을 비교해서 뒤의 index값이 작다면 둘의 자리를 바꾼다.
+                    int tmp = arr[j];  // 2개의 자리를 바꾸는 코딩
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                }
+            }
+        }
         return arr;
     }
     public static void main(String[] args) {
