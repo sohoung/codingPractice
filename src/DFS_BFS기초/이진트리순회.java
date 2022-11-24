1daea97663b54860265c3e685d971e4c7681a566
김민성 <sohoung4@gmail.com>
2022-11-24 21:45:08 +0900

    이진트리순회

--- src/DFS_BFS기초/이진트리순회.java
+++ src/DFS_BFS기초/이진트리순회.java
@@ -7,13 +7,20 @@ class Node {
     Node lt,rt;  // 왼쪽 자식의 노드 주소와 오른쪽 자식의 노드 주소를 저장하는 변수
     public Node(int val) {
         data = val;
-        lt=rt=null;
+        lt=rt=null;  // 객체가 생성되는 순간에는 lt와 rt는 null 값이다.
     }
 }
 public class 이진트리순회 {
     Node root;
     public void DFS(Node root) {
-
+        if(root==null) return;
+        else {
+            System.out.print(root.data + " ");  // 전위
+            DFS(root.lt);
+            //System.out.print(root.data + " ");  // 중위
+            DFS(root.rt);
+            //System.out.print(root.data + " ");  // 후위
+        }
     }
     public static void main(String[] args) {
         이진트리순회  m = new 이진트리순회();
