1fa1e79cc947f6c21b4edd85621e635c70f876ec
김민성 <sohoung4@gmail.com>
2022-11-27 16:35:15 +0900

    이진트리레벨탐색

---
+++ src/DFS_BFS기초/이진트리레벨탐색.java
@@ -0,0 +1,11 @@
+package DFS_BFS기초;
+
+// 설명 : 이진트리 레벨탐색을 연습하세요
+// 입력 예시 : 
+
+public class 이진트리레벨탐색 {
+    public static void main(String[] args) {
+        이진트리레벨탐색  m = new 이진트리레벨탐색();
+
+    }
+}
