3baafca5f6b0466a29a23f25abe6ed6411d5094e
김민성 <sohoung4@gmail.com>
2022-11-30 20:55:15 +0900

    경로탐색 인접리스트

---
+++ src/DFS_BFS기초/경로탐색_인접리스트.java
@@ -0,0 +1,13 @@
+package DFS_BFS기초;
+import java.util.Scanner;
+
+// 설명 : 방향그래프가 주어지면 1번 정점에서 N번 정점으로 가는 모든 경로의 가지 수를 출력하는 프로그램을 작성하세요.
+// 입력 예시 :
+// 출력 예시 : 
+public class 경로탐색_인접리스트 {
+    public static void main(String[] args) {
+        경로탐색_인접리스트  m = new 경로탐색_인접리스트();
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
