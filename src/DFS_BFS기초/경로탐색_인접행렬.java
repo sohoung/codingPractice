0b5b92c3f50b73b0a450e3bffd328adb5943ad55
김민성 <sohoung4@gmail.com>
2022-11-30 21:57:20 +0900

    경로 탐색 인접행렬 이용

--- src/DFS_BFS기초/경로탐색_인접행렬.java
+++ src/DFS_BFS기초/경로탐색_인접행렬.java
@@ -13,20 +13,22 @@ import java.util.Scanner;
 //           4 2
 //           4 5
 // 출력 예시 : 6
+// * 방문한 노드를 또 방문하는 것은 없다.
 public class 경로탐색_인접행렬 {
     static int n, k, answer = 0;
-    static int[][] graph;
-    static int[] ch;
+    static int[][] graph;  // 입력받은 그래프를 2차원 배열로 표현
+    static int[] ch;  // 방문 했는지에 대한 체크 배열
     public void DFS(int v) {
-        if(v==n) {
+        if(v==n) {  // 종점에 왔을 때
             answer++;
         }
         else {
             for(int i = 1; i <= n; i++) {
-                if(graph[v][i] == 1 && ch[i] == 0) {
-                    ch[i] = 1;
-                    DFS(i);
-                    ch[i] = 0;
+                if(graph[v][i] == 1 && ch[i] == 0) {  // 한 번도 방문하지 않고 graph배열에 있는 방향이라면
+                    ch[i] = 1;  // 값을 넣어주고 체크
+                    DFS(i);     // 백 트래킹
+                    ch[i] = 0;  // 백 트래킹을 해줄 때 체크를 풀어줘야 다른 방향으로 갈 수 있다. 체크를 풀어주지 않으면 이미 체크된 지점이라고 판단하고
+                                // if문을 수행하지 않을 수 있다.
                 }
             }
         }
