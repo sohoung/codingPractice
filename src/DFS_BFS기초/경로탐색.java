e43fcdc7864d2f9dc5799cd3fef44813d5a1090e
김민성 <sohoung4@gmail.com>
2022-11-30 20:23:58 +0900

    경로 탐색

---
+++ src/DFS_BFS기초/경로탐색.java
@@ -0,0 +1,8 @@
+package DFS_BFS기초;
+
+// 설명 : 방향 그래프가 주어지면 1번 정점에서 N번 정점으로 가는 모든 경로의 가지 수를 출력하는 프로그램을 작성하세요.
+public class 경로탐색 {
+    public static void main(String[] args) {
+        경로탐색  m = new 경로탐색();
+    }
+}
