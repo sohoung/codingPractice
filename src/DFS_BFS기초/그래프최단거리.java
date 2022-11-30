a2a2da91708453871e73b8f9bf3c6d4806d41a6a
김민성 <sohoung4@gmail.com>
2022-11-30 21:57:02 +0900

    그래프 최단 거리

---
+++ src/DFS_BFS기초/그래프최단거리.java
@@ -0,0 +1,12 @@
+package DFS_BFS기초;
+import java.util.Scanner;
+
+// 설명 : 다음 그래프에서 1번 정점에서 ㄱ각 정점으로 가는 최소 이동 간선 수를 구하세요.
+// 입력 예시 :
+// 출력 예시 : 
+public class 그래프최단거리 {
+    public static void main(String[] args) {
+        그래프최단거리 m = new 그래프최단거리();
+        Scanner sc = new Scanner(System.in);
+    }
+}
