8973d5d028ff5369d9ada9d2e8db3fc430ff6e6b
김민성 <sohoung4@gmail.com>
2022-11-30 20:24:08 +0900

    그래프와 인접 행렬

--- src/DFS_BFS기초/그래프와인접행렬.java
+++ src/DFS_BFS기초/그래프와인접행렬.java
@@ -1,5 +1,7 @@
 package DFS_BFS기초;
 
+// 그래프를 표기 할 때 G(V,E)로 표기하고 V는 노드를 뜻하고 E는 간선을 뜻한다.
+
 public class 그래프와인접행렬 {
     public static void main(String[] args) {
         그래프와인접행렬  m = new 그래프와인접행렬();
