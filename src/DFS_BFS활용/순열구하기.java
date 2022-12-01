ae3ae98b3b265c4ac268f102002909575fa836ec
김민성 <sohoung4@gmail.com>
2022-12-01 22:17:06 +0900

    순열 구하기

---
+++ src/DFS_BFS활용/순열구하기.java
@@ -0,0 +1,12 @@
+package DFS_BFS활용;
+import java.util.Scanner;
+
+// 설명 : 10이하의 N개의 자연수가 주어지면 이 중 K개를 뽑아 일렬로 나열하는 방법을 모두 출력하세요.
+// 입력 예시 : 
+public class 순열구하기 {
+    public static void main(String[] args) {
+        순열구하기  m = new 순열구하기();
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
