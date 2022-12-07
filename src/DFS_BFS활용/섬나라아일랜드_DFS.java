161b793fa0e449692575651400f14b2047861cbe
김민성 <sohoung4@gmail.com>
2022-12-07 20:59:39 +0900

    섬나라 아일랜드 DFS

---
+++ src/DFS_BFS활용/섬나라아일랜드_DFS.java
@@ -0,0 +1,22 @@
+package DFS_BFS활용;
+import java.util.Scanner;
+
+// 설명 : N*N의 섬나라 아일랜드의 지도가 격자판의 정보로 주어집니다.
+//       각 섬은 1로 표시되어 상하좌우와 대각선으로 연결되어 있으며, 0은 바다입니다.
+//       섬나라 아일랜드에 몇 개의 섬이 있는지 구하는 프로그램을 작성하세요.
+// 입력 예시 : 7
+//           1 1 0 0 0 1 0
+//           0 1 1 0 1 1 0
+//           0 1 0 0 0 0 0
+//           0 0 0 1 0 1 1
+//           1 1 0 1 1 0 0
+//           1 0 0 0 1 0 0
+//           1 0 1 0 1 0 0
+// 출력 예시 : 5
+public class 섬나라아일랜드_DFS {
+    public static void main(String[] args) {
+        섬나라아일랜드_DFS  m = new 섬나라아일랜드_DFS();
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
