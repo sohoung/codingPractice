fa0c438bf9f4a5cb8548f1b0b35c9153ccc3f97e
김민성 <sohoung4@gmail.com>
2022-12-08 21:43:42 +0900

    피자 배달 거리

--- src/DFS_BFS활용/피자배달거리.java
+++ src/DFS_BFS활용/피자배달거리.java
@@ -29,7 +29,25 @@ public class 피자배달거리 {
     static int[] combi;
     static ArrayList<Point> pz,hs;
     public void DFS(int L, int s) {
-
+        if(L == k) {  // 조합이 완성 되었을 때
+            int sum = 0;  // 도시의 피자배달거리
+            for(Point h : hs) {  // 입력된 집에 좌표 대응
+                int dis = Integer.MAX_VALUE;  // 최솟 값을 구해야 하므로 Intger.MAX_VALUE사용
+                for(int i : combi) {  // 조합의 index 번호가 i에 하나하나 대응
+                    dis = Math.min(dis,Math.abs(h.x - pz.get(i).x) + Math.abs(h.y - pz.get(i).y));
+                    // 집의 x좌표와 피자집의 x좌표를 빼주고 절댓값을 해주어서 집이 하나 결정되면 살아남은 피자집을 모두 비교해서 거리들 중에서 제일 최소 거리를 구한다.
+                    // 즉 대응된 하나의 집에 대한 피자 배달 거리를 구한다.
+                }
+                sum += dis;  // 도시의 피자 배달 거리
+            }
+            answer = Math.min(answer, sum);  // 최솟 값을 구해야 하므로 Math.min함수로 answer와 sum 중 더 작은 값으로 answer에 갱신
+        }
+        else {
+            for(int i = s; i < len; i++) {
+                combi[L] = i;
+                DFS(L + 1, i + 1);
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -51,7 +69,7 @@ public class 피자배달거리 {
             }
         }
         len = pz.size();  // 도시에 있는 피자집에 갯수
-        combi = new int[k];
+        combi = new int[k];  // len Combi k 를구한다
         m.DFS(0,0);
         System.out.println(answer);
     }
