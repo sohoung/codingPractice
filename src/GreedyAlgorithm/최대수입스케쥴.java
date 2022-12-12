6c573634444faabedd6798895a16813cae117ee1
김민성 <sohoung4@gmail.com>
2022-12-12 19:29:21 +0900

    최대 수입 스케쥴 PriorityQueue 사용

--- src/GreedyAlgorithm/최대수입스케쥴.java
+++ src/GreedyAlgorithm/최대수입스케쥴.java
@@ -19,36 +19,46 @@ import java.util.Scanner;
 // compareTo 정의 기준 : 매개 변수(o) - this 변수 = 내림차순 정렬
 //                    this 변수 - 매개 변수(o) = 오름차순 정렬
 
-class Lexture implements Comparable<Lexture> {
+class Lecture implements Comparable<Lecture> {
     public int money;
-    public int time;
-    Lexture(int money, int time) {
+    public int date;
+    Lecture(int money, int date) {
         this.money = money;
-        this.time = time;
+        this.date = date;
     }
 
     @Override
-    public int compareTo(Lexture o) {
-        return o.time - this.time;  // time에 의해 내림차순 정렬
+    public int compareTo(Lecture o) {
+        return o.date - this.date;  // date에 의해 내림차순 정렬
     }
 }
 public class 최대수입스케쥴 {
-    static int n,max = Integer.MIN_VALUE;
-    public int solution(ArrayList<Lexture> arr) {
+    static int n,max = Integer.MIN_VALUE;  // 최댓값을 구해주어야하므로 Integer.MIN_VALUE로 최솟값으로 초기화
+    public int solution(ArrayList<Lecture> arr) {
         int answer = 0;
-        PriorityQueue<Integer> p = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> p = new PriorityQueue<>(Collections.reverseOrder());  // PriorityQueue 객체 생성
+        // Collections.reverseOrder가 없다면 PritorityQueue에서 제일 작은 값을 poll하지만 Collections.reverseOrder가 있다면 반대로 정렬하므로 제일 큰 값을 poll해준다.
+        Collections.sort(arr);  // 재정의 했던 date에 의한 내림차순 정렬
+        int j = 0;
+        for(int i = max; i >= 1; i--) {  // max 날짜부터 1까지 for문 수행
+            for( ; j < n; j++) {
+                if(arr.get(j).date < i) break;  // 만약 max의 날짜보다 arr.get(j)date의 날짜가 크다면 더이상 p에 money를 넣지 않아야 하므로 break
+                p.offer(arr.get(j).money);  // max의 날짜보다 arr.get(j).date의 날짜가 작다면 p에 money를 넣어준다.
+            }
+            if(!p.isEmpty()) answer += p.poll();
+            // p가 비어있지 않다면 answer에 p에 들어있는 값들 중에 설정되어있는 우선순위에서 제일 적합한 값을 p에 poll로 return시켜준다.
+        }
         return answer;
-
     }
     public static void main(String[] args) {
         최대수입스케쥴 M = new 최대수입스케쥴();
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        ArrayList<Lexture> arr = new ArrayList<>();  // Lecture 객체를 저장하는 ArrayList 객체 생성
+        n = sc.nextInt();  // 전역변수로 이미 선언되어있으므로 int를 붙이지 않는다. int를 붙여서 원하는 출력값이 나오지 않아서 찾는데 힘들었다. 개념 다시 숙지
+        ArrayList<Lecture> arr = new ArrayList<>();  // Lecture 객체를 저장하는 ArrayList 객체 생성
         for(int i = 0; i < n; i++) {
             int m = sc.nextInt();
             int d = sc.nextInt();
-            arr.add(new Lexture(m,d));
+            arr.add(new Lecture(m,d));
             if(d > max) max = d;  // 날짜 중 제일 큰 값을 max에 넣어준다.
         }
         System.out.println(M.solution(arr));
