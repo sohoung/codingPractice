a42a2df113242c3c4c96af74a725ccbc16f29143
김민성 <sohoung4@gmail.com>
2022-12-12 19:29:31 +0900

    다익스트라알고리즘

---
+++ src/GreedyAlgorithm/다익스트라알고리즘.java
@@ -0,0 +1,11 @@
+package GreedyAlgorithm;
+import java.util.Scanner;
+
+// 설명 : 
+public class 다익스트라알고리즘 {
+    public static void main(String[] args) {
+        다익스트라알고리즘 m = new 다익스트라알고리즘();
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
