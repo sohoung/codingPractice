ffd8fb6768b74c31b4f95710a33e57e6873ab64f
김민성 <sohoung4@gmail.com>
2022-12-15 15:23:16 +0900

    원더랜드 프림 PriorityQueue

---
+++ src/GreedyAlgorithm/원더랜드_프림.java
@@ -0,0 +1,11 @@
+package GreedyAlgorithm;
+import java.util.Scanner;
+
+// 설명 :
+// PriorityQueue
+public class 원더랜드_프림 {
+    public static void main(String[] args) {
+        원더랜드_프림  m = new 원더랜드_프림();
+        Scanner sc = new Scanner(System.in);
+    }
+}
