027c03cf94bfa6a8db2bf61bbff658474eebbf8f
김민성 <sohoung4@gmail.com>
2022-12-17 22:01:20 +0900

    돌다리 건너기

---
+++ src/동적계획법/돌다리건너기.java
@@ -0,0 +1,14 @@
+package 동적계획법;
+import java.util.Scanner;
+
+// 설명 : 철수는 학교에 가는데 개울을 만났습니다. 개울은 N개의 돌로 다리를 만들어 놓았습니다.
+//       철수는 돌 다리를 건널 때 한 번에 한 칸 또는 두 칸씩 건너뛰면서 돌다리를 건널 수 있습니다.
+//       철수가 개울을 건너는 방법은 몇 가지일까요?
+// 입력 예시 : 7
+// 출력 예시 : 34
+public class 돌다리건너기 {
+    public static void main(String[] args) {
+        돌다리건너기  m = new 돌다리건너기();
+        Scanner sc = new Scanner(System.in);
+    }
+}
