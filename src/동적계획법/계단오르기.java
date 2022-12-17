40a8e6a9fa29bca3dca7eed7d8a74e2a48ed2d95
김민성 <sohoung4@gmail.com>
2022-12-17 22:01:14 +0900

    계단오르기

---
+++ src/동적계획법/계단오르기.java
@@ -0,0 +1,15 @@
+package 동적계획법;
+import java.util.Scanner;
+
+// 설명 : 철수는 계단을 오를 때 한 번에 한 계단 또는 두 계단씩 올라간다. 만약 총 4계단을 오른다면 그 방법의 수는
+//       1+1+1+1, 1+1+2, 1+2+1, 2+1+1, 2+2 로 5가지이다.
+//       그렇다면 총 N계단일 때 철수가 올라갈 수 있는 방법의 수는 몇 가지인가?
+// 입력 예시 : 7
+// 출력 예시 : 21
+public class 계단오르기 {
+    public static void main(String[] args) {
+        계단오르기  m = new 계단오르기();
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
