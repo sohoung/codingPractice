0c80e10839877acc3d144c0c6dc9418c6dfaca90
김민성 <sohoung4@gmail.com>
2022-12-22 14:44:46 +0900

    돌다리 건너기

--- src/동적계획법/돌다리건너기.java
+++ src/동적계획법/돌다리건너기.java
@@ -7,8 +7,22 @@ import java.util.Scanner;
 // 입력 예시 : 7
 // 출력 예시 : 34
 public class 돌다리건너기 {
+    static int[] dy;
+    public int solution(int n) {
+        dy[1] = 1;
+        dy[2] = 2;
+        for(int i = 3; i <= n+1; i++) {  // 마지막 돌다리에서 건너편 땅까지 가는 방법도 구해우어야하니까 실행 횟수를 n+1까지 실행할 수 있도록 조건을 걸어준다.
+            dy[i] = dy[i-1] + dy[i-2];
+        }
+        return dy[n+1];
+    }
     public static void main(String[] args) {
         돌다리건너기  m = new 돌다리건너기();
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        dy = new int[n+2];
+        // 배열의 크기를 n+2로 잡은 이유는 문제의 조건은 돌다리를 건너는 방법이므로 마지막 돌다리에서 건너편 땅까지 가는 방법이다. 즉 마지막 돌다리에서도 건너편 땅까지 가는 방법을 구해야 하는 것이다.
+        // 이 말은 solution 함수의 for문에서 for문의 실행 횟수에 대한 조건을 걸어 줄 때 마지막 돌다리에서 건너편 땅까지 가는 방법도 구해야하므로 전의 계단 오르기보다 1번 더 답을 구해야한다.
+        System.out.println(m.solution(n));
     }
 }
