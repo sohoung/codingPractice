124933c9ff939927325dbffdc3847d612879085e
김민성 <sohoung4@gmail.com>
2022-12-26 16:03:00 +0900

    최대 점수 구하기 냅색 알고리즘

--- src/동적계획법/최대점수구하기_냅색알고리즘.java
+++ src/동적계획법/최대점수구하기_냅색알고리즘.java
@@ -18,5 +18,19 @@ public class 최대점수구하기_냅색알고리즘 {
     public static void main(String[] args) {
         최대점수구하기_냅색알고리즘  m = new 최대점수구하기_냅색알고리즘();
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();  // 문제의 갯수
+        int k = sc.nextInt();  // 제한 시간
+        int[] dy = new int[k+1];
+        for(int i = 0; i < n; i++) {
+            int ps = sc.nextInt();  // 문제를 풀면 얻는 점수
+            int pt = sc.nextInt();  // 문제를 풀기 위한 시간
+            for(int j = k; j >= pt; j--) {
+                dy[j] = Math.max(dy[j], dy[j-pt] + ps);
+                // dy[j-pt] + ps에서 ps를 더해주는 이유는 pt라는 문제 푸는 시간이라는 것이 있다. 이것은 이 문제를 무조건 푼다는 가정으로 생기는 코드이다.
+                // 즉 pt에 대한 문제를 무조건 푼다는 가정이 있으므로 그 문제에 대한 점수를 받아야 한다는 것이다. 그러므로 무조건 그 문제는 푼다는 가정이라는 결론이 있으므로 그 문제에 대한 점수인 ps를
+                // 더해준다.
+            }
+        }
+        System.out.println(dy[k]);
     }
 }
